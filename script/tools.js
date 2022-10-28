eec0c5fc4b60f2915784611ad22ede22bca68de5
emiliamassing <emiliamassings@gmail.com>
2022-10-28 12:42:05 +0200

    Added code to close menu when clicking on link

--- script/tools.js
+++ script/tools.js
@@ -1,6 +1,14 @@
 const menuButton = document.getElementsByClassName('toggleMenu')[0]
 const menuLinks = document.getElementsByClassName('menuLinks')[0]
 
+//Ger menuLinks klassen active när man klickar på knappen vilket öppnar menyn
 menuButton.addEventListener('click', () =>{
     menuLinks.classList.toggle('active')
 })
+
+//Tar bort active när man klickar på en av länkarna. Gäller alla länkar
+document.querySelectorAll('.listItem').forEach(n => n.
+    addEventListener('click', () => {
+        menuButton.classList.remove('active');
+        menuLinks.classList.remove('active');
+    }))
