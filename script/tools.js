037cc1a40868efe75c743d320705182b3b36a9be
emiliamassing <emiliamassings@gmail.com>
2022-10-28 13:48:12 +0200

    solved menu problem

--- script/tools.js
+++ script/tools.js
@@ -1,14 +1,21 @@
 const menuButton = document.getElementsByClassName('toggleMenu')[0]
 const menuLinks = document.getElementsByClassName('menuLinks')[0]
 
+let menuOpen = false;
+
 //Ger menuLinks klassen active när man klickar på knappen vilket öppnar menyn
 menuButton.addEventListener('click', () =>{
-    menuLinks.classList.toggle('active')
+    menuOpen = !menuOpen;
+    if(!menuOpen) menuButton.blur();  //"Tar bort" focus från knappen 
+    menuLinks.classList.toggle('active');
+    menuButton.classList.toggle('active');
 })
 
 //Tar bort active när man klickar på en av länkarna. Gäller alla länkar
 document.querySelectorAll('.listItem').forEach(n => n.
     addEventListener('click', () => {
+        menuOpen = false;
+        menuButton.blur();
         menuButton.classList.remove('active');
         menuLinks.classList.remove('active');
-    }))
+}))
